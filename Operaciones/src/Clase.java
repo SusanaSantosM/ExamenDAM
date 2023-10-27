10b669b9725931e0968c1171b8a898fa7a33dcbf
ssusibel <dam1@aulaAulainformatica1>
2023-10-27 09:19:06 +0200

    añado la clase del nuevo programa

---
+++ Operaciones/src/Clase.java
@@ -0,0 +1,10 @@
+public class Clase {
+    public void verClase(short num1,short num2) {
+        double resta = num1 - num2;
+        double suma = num1 + num2;
+
+        if (num1 >= num2)
+            System.out.println("Vemos la resta" + resta);
+        System.out.println("Vemos la suma" + suma);
+    }
+}
